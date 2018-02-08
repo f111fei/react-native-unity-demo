f589742e55a05aab0d4a0ad78c2077e8001e4663
xzper <mail@xzper.com>
2018-02-08 16:20:35 +0800

    fix unity build

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  Button
 } from 'react-native';
 
 import UnityView from 'react-native-unity-view';
@@ -22,10 +23,29 @@ const instructions = Platform.select({
 });
 
 type Props = {};
-export default class App extends React.Component<Props> {
+
+type State = {
+  unity: boolean;
+};
+
+export default class App extends React.Component<Props, State> {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      unity: false
+    }
+  }
+
+  private onPress() {
+    this.setState({ unity: !this.state.unity });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
+      <View style={[styles.container, { backgroundColor: this.state.unity ? "rgba(0,0,0,0)" : "red" }]}>
+        {/* {this.state.unity ? <UnityView style={{ position: 'absolute', top: 0, bottom: 0, left: 0, right: 0 }} /> : null} */}
+        {this.state.unity ? <UnityView style={{ position: 'absolute', left: 0, right: 0, top: 1, bottom: 1, }} /> : null}
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
@@ -35,7 +55,8 @@ export default class App extends React.Component<Props> {
         <Text style={styles.instructions}>
           {instructions}
         </Text>
-        <UnityView style={{ width: 300, height: 300 }}/>
+        {/* {this.state.unity ? <UnityView style={{ position: 'absolute', width: 300, height: 300 }} /> : null} */}
+        <Button title="切换显示Unity" onPress={this.onPress.bind(this)} />
       </View>
     );
   }
@@ -43,7 +64,8 @@ export default class App extends React.Component<Props> {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    // flex: 1,
+    position: 'absolute', top: 0, bottom: 0, left: 0, right: 0,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
